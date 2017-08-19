9b59da4cea3eeac35fc36f7f45353bfccc4cd81a
Chris Larsen <clarsenipod@gmail.com>
2017-08-18 20:24:47 -0400

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -9,18 +9,21 @@ class App extends Component {
         <div className="header">
           <h1>Chris Larsen</h1>
           <nav>
-            <ul>
-              <li>Home</li>
-              <li>Freelance Work</li>
-              <li>Open Source Work</li>
-              <li>Side Projects</li>
-              <li>Blog</li>
+            <ul className="navbar navbar-left">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Freelance Work</a></li>
+              <li><a href="#">Open Source Work</a></li>
+              <li><a href="#">Side Projects</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+            <ul className="navbar navbar-right">
+              <li><a href="#">Sign In</a></li>
             </ul>
           </nav>
         </div>
-        <p className="Content">
-          My personal page.
-        </p>
+        <section>
+          <p className="Content">{"I'm Chris Larsen, a freelance web developer, electronics technologist, rubyist, and general technology fanatic."}</p>
+        </section>
       </div>
     );
   }
