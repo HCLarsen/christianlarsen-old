a08fee09f7b841472949c666b7746b1c47c4a323
Chris Larsen <clarsenipod@gmail.com>
2017-09-19 02:03:04 -0400

    Add External Links

--- src/App.js
+++ src/App.js
@@ -18,7 +18,20 @@ class App extends Component {
     return (
       <div className="App">
         <div className="header">
-          <h1>Chris Larsen</h1>
+          <div className="title-bar">
+            <h1 className="title">Chris Larsen</h1>
+            <div className="contact-info">
+              <ul className="contact-info">
+                <li><a href="https://www.linkedin.com/in/chris-larsen-58347214/" target="_blank">LinkedIn</a></li>
+                <li><a href="https://github.com/HCLarsen">Github</a></li>
+              </ul>
+              <ul className="contact-info">
+                <li><a href="https://www.hackerrank.com/HCLarsen" target="_blank">HackerRank</a></li>
+                <li><a href="https://codefights.com/profile/hclarsen" target="_blank">CodeFights</a></li>
+                <li><a href="https://www.codingame.com/profile/ef9dbf586fe48edbd0ca1e569084a33f2941112" target="_blank">CodinGame</a></li>
+              </ul>
+            </div>
+          </div>
           <nav>
             <ul className="navbar navbar-left">
               <li><a>Home</a></li>
