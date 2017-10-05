fb43ce0a37f2491741f5e85c6614ae5ffcfee870
Chris Larsen <clarsenipod@gmail.com>
2017-10-05 13:42:56 -0400

    Move text to JSON files

--- src/Freelance.js
+++ src/Freelance.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import Project from './Project';
+import text from './freelance.json'
 
 class Freelance extends Component {
-  constructor() {
-    super();
-    this.state = {
-      projects: [{ name: "Electric Star Productions", url: "https://www.electricstarproductions.ca/", description: "Electric Star Productions is a DJ and Events company in Toronto. They wanted a website with editable information about their DJs. I used ReactJS on the front end, to provide smooth transition between the standard view and the forms that enable editing. The back end uses Ruby on Rails to store the content in a PostgreSQL Database."}]
-    }
-  }
   render() {
-    var projects = this.state.projects.map(function(p) {
-      return <Project name={p.name} url={p.url} description={p.description} />
+    var projects = text.map(function(p, i) {
+      return <Project key={i} name={p.name} url={p.url} description={p.description} />
     });
     return(
       <section className="content">
