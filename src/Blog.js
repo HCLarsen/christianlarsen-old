05407fb300c2c9dbe9f7ec62a9643282031c3ef5
Chris Larsen <clarsenipod@gmail.com>
2017-10-13 01:48:44 -0400

    Add Index to Blog

--- src/Blog.js
+++ src/Blog.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Markdown from 'react-remarkable'
+import BlogIndex from './BlogIndex'
+
+var input = '# This is a header\n\nAnd this is a paragraph';
 
 class Blog extends Component {
   render() {
     return(
       <section className="content">
-        <p>Blog Is Under Construction</p>
+        <article className="blog-content">
+          <Markdown source={input} />
+        </article>
+        <BlogIndex />
       </section>
     );
   }
