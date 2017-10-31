f2ccb048af9d2b831ecbcee1f5a48dd96c14dc5d
Chris Larsen <clarsenipod@gmail.com>
2017-10-31 15:14:16 -0400

    Add Github Interface

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import Home from './Home';
 import Freelance from './Freelance';
 import OpenSource from './OpenSource';
 import Projects from './Projects';
-import Blog from './blog/Blog';
+import Client from './Client'
 import './App.css';
 import { Link, Switch, Route } from 'react-router-dom'
 
+const CONTRIBUTIONS_LIMIT = 2 // maximum number of open source contributions displayed.
+const ORIGINAL_LIMIT = 5 // maximum number of personal projects displayed.
+
 class App extends Component {
+  state = {
+    projects: [],
+    contributions: []
+  }
+  componentWillMount() {
+    console.log("Mounting")
+    Client.search(repos => {
+      var projects = repos.filter(function(repo) {
+        return !repo.fork && repo.stargazers_count > 0
+      }).slice(0, ORIGINAL_LIMIT)
+      var contributions = repos.filter(function(repo) {
+        return repo.fork
+      }).slice(0, CONTRIBUTIONS_LIMIT)
+      this.setState({
+        projects: projects,
+        contributions: contributions
+      })
+      console.log(this.state.projects)
+      console.log(this.state.contributions)
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -23,10 +47,6 @@ class App extends Component {
               <li><Link to="/freelance">Freelance Work</Link></li>
               <li><Link to="/openSource">Open Source</Link></li>
               <li><Link to="/projects">Side Projects</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
-            </ul>
-            <ul className="navbar navbar-right">
-              <li><a>Sign In</a></li>
             </ul>
           </nav>
         </div>
@@ -34,9 +54,12 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/freelance" component={Freelance} />
-            <Route path="/opensource" component={OpenSource} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/blog" component={Blog} />
+            <Route path="/opensource" render={(props) => (
+              <OpenSource projects={this.state.contributions} {...props} />
+            )} />
+            <Route path="/projects" render={(props) => (
+              <Projects projects={this.state.projects} {...props} />
+            )} />
           </Switch>
         </div>
       </div>
--- src/OpenSource.js
+++ src/OpenSource.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import Project from './Project';
-import text from './OpenSource.json'
 
 class OpenSource extends Component {
   render() {
-    var projects = text.map(function(p, i) {
-      return <Project key={i} name={p.name} url={p.url} description={p.description} />
+    var projects = this.props.projects.map(function(p, i) {
+      return <Project key={i} name={p.name} url={p.html_url} description={p.description} target="_blank" rel="noopener noreferrer" />
     });
     return(
       <section className="content">
