0827d4b5e58e956dec7c38ace9f0ed22ee955398
Chris Larsen <clarsendev@protonmail.com>
2020-03-09 23:16:01 -0400

    Updates

---
+++ precache-manifest.275099db86b4f9b5fde03e067fdfceb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06a817fc7c5717fb61d4",
+    "url": "/static/css/main.3d8d5409.chunk.css"
+  },
+  {
+    "revision": "06a817fc7c5717fb61d4",
+    "url": "/static/js/main.5ff9be36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2232b695426af6eea872",
+    "url": "/static/js/2.e7c5f8b6.chunk.js"
+  },
+  {
+    "revision": "34e5b50200cc92a269caceec2acc9a2c",
+    "url": "/static/media/menu-icon.34e5b502.png"
+  },
+  {
+    "revision": "7dc4af164da709a4416e919dec3b394d",
+    "url": "/index.html"
+  }
+];
